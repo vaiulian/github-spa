79b8271c977a1a13525bfa1e637398fd50c958ca
vaiulian <vasileaiulian@gmail.com>
2018-05-16 16:27:46 +0300

    Update Home text

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -8,6 +8,22 @@ import React from 'react';
  */
 
 
-const Home = () => <h2>Welcome Home</h2>;
+const Home = () => (<div>
+  <h2>Welcome</h2>
+  <p>This is an application that using the github developers API will
+        fetch and show a basic user Profile.
+  </p>
+  <p>Fell free to check the menu for the Profile and Compare pages</p>
+  <p>This project uses:</p>
+  <ol className="enum">
+    <li>ReactJS</li>
+    <li>React-create-app as scaffolding</li>
+    <li>Redux</li>
+    <li>Thunk</li>
+    <li>React router</li>
+    <li>Material UI</li>
+    <li>Other react libs and plugins (package.json)</li>
+  </ol>
+</div>); // eslint-disable-line
 
 export default Home;
